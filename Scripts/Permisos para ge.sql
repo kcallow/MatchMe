b9c3d6dca16a0b340ed2e5b6e087bc12452b0520
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-09-11 23:54:14 -0600

    Remover espacios en nombres de archivos.

--- Scripts/Permisos para ge.sql
+++
@@ -1,9 +0,0 @@
-grant create session to ge;
-grant create table to ge;
-grant create session to ge;
-
-grant select on V_$SESSION  to ge;
-grant select on V_$sesstat  to ge;
-grant select on V_$STATNAME  to ge;
-
-grant create view to ge;
