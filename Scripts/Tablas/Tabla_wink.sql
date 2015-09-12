d24ca2c43c657196667a59781f306ba04f7ec893
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-09-12 01:14:59 -0600

    Winks

--- Scripts/Tablas/Tabla_wink.sql
+++ Scripts/Tablas/Tabla_wink.sql
@@ -1,28 +1,13 @@
--- Create table
 create table WINK
 (
-  WINK_id           NUMBER not null,
-  fec_WINK                    DATE,
-  fec_creacion                DATE,
-  fec_ultima_modificacion     DATE,
-  usuario_creacion            VARCHAR2(10),
-  usuario_ultima_modificacion VARCHAR2(10)
-)
-tablespace GE_DATA
-  pctfree 10
-  initrans 1
-  maxtrans 255
-  storage
-  (
-    initial 64K
-    minextents 1
-    maxextents unlimited
-  );
--- Create/Recreate primary, unique and foreign key constraints 
-alter table WINK
-  add constraint PK_WINK primary key (WINK_ID)
   using index 
-  tablespace GE_DATA
-  pctfree 10
-  initrans 2
-  maxtrans 255;
+  tablespace GE_DATA pctfree 10
+  storage ( initial 64K minextents 1 maxextents unlimited),
+  WINK_id		   	NUMBER,
+  constraint PK_WINK primary key (WINK_ID),
+  fec_WINK			DATE,
+  fec_creacion			DATE,
+  fec_ultima_modificacion	DATE,
+  id_usuario_emisor		VARCHAR2(10),
+  id_usuario_receptor	 	VARCHAR2(10)
+)
