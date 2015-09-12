c519109423cccffe3fc5ca4365c32ac150adfdfe
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-09-12 09:50:14 -0600

    FK nombres explicitos.

--- Scripts/3_Tablas/1_PersonaDisponible/wink.sql
+++ Scripts/3_Tablas/1_PersonaDisponible/wink.sql
@@ -4,8 +4,8 @@ create table WINK
 --  fec_creacion			DATE,
 --  fec_ultima_modificacion	DATE,
   username_emisor		VARCHAR2(10),
-  CONSTRAINT fk_emisor FOREIGN KEY (username_emisor) REFERENCES PERSONA(username),
+  CONSTRAINT fk_wink_persona_emisor FOREIGN KEY (username_emisor) REFERENCES PERSONA(username),
   username_receptor	 	VARCHAR2(10),
-  CONSTRAINT fk_receptor FOREIGN KEY (username_receptor) REFERENCES PERSONA(username),
+  CONSTRAINT fk_wink_persona_receptor FOREIGN KEY (username_receptor) REFERENCES PERSONA(username),
   CONSTRAINT pk_wink PRIMARY KEY (fec_WINK, username_emisor, username_receptor)
 )
