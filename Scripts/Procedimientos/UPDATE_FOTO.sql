1679f16fc7b69c7d16ecf6f8788b227450c9c89d
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-09-13 22:30:36 -0600

    Updates arreglados

--- Scripts/Procedimientos/UPDATE_FOTO.sql
+++ Scripts/Procedimientos/UPDATE_FOTO.sql
@@ -1,6 +1,6 @@
-create or replace procedure UPDATE_FOTO(pRUTA IN VARCHAR, pFOTO_ID IN NUMBER) is
+create or replace procedure UPDATE_FOTO(pNOMBRE IN VARCHAR, pRUTA IN VARCHAR, pFOTO_ID IN NUMBER) is
 begin
   update FOTO
-    set RUTA = pRUTA
+    set NOMBRE = pNOMBRE, RUTA = pRUTA
       where FOTO_id = pFOTO_ID;
         end UPDATE_FOTO;
