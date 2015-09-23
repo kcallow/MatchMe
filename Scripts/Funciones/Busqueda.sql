d48e9dd19fa03ffbacf3bd4edacfc0d9db67ad0a
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-09-23 01:06:05 -0600

    Merge!

--- Scripts/Funciones/Busqueda.sql
+++ Scripts/Funciones/Busqueda.sql
@@ -3,28 +3,5 @@ CREATE OR REPLACE FUNCTION fecha_to_edad(fecha IN DATE)
 	AS edad DATE
 BEGIN
 	RETURN trunc(months_between(sysdate,fecha)/12);
-END;
---
-CREATE OR REPLACE FUNCTION consulta_edad_min(edadMin IN number)
-	RETURN sys_refcursor
-	AS resultado sys_refcursor;
-
-BEGIN
-	OPEN resultado FOR
-	SELECT username
-	FROM persona
-	WHERE fecha_to_edad(fecha) > edadMin;
-	RETURN resultado;
-END;
---
-CREATE OR REPLACE FUNCTION consulta_edad_max(edadMax IN number)
-	RETURN sys_refcursor
-	AS resultado sys_refcursor;
-
-BEGIN
-	OPEN resultado FOR
-	SELECT username
-	FROM persona
-	WHERE fecha_to_edad(fecha) < edadMax;
-	RETURN resultado;
-END;
+END fecha_to_edad;
+/
