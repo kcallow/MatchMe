3f5915f9ba563443887efc054f818582153d77a9
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-09-26 21:37:20 -0600

    Persona disponible insert

---
+++ Scripts/DML/Datos/SH/InsertPersona_Disponible.sh
@@ -0,0 +1,28 @@
+line=""
+getField(){
+	echo "$line" | cut -f${1}
+}
+
+echo "INSERT ALL"
+while read line
+do
+username=$(getField 1)
+es_real=$(getField 2)
+color_ojos_id=$(getField 3)
+color_piel_id=$(getField 4)
+contextura_id=$(getField 5)
+nivel_educacion_id=$(getField 6)
+estado_civil_id=$(getField 7)
+fumador_id=$(getField 8)
+bebedor_id=$(getField 9)
+frecuencia_ejercicio_id=$(getField 10)
+salario_id=$(getField 11)
+altura=$(getField 12)
+peso=$(getField 13)
+num_hijos=$(getField 14)
+quiere_hijos=$(getField 15)
+cat <<- EOF
+INTO PERSONA_DISPONIBLE (username, es_real, color_ojos_id, color_piel_id, contextura_id, nivel_educacion_id, estado_civil_id, fumador_id, bebedor) VALUES ('${username}', '${es_real}', ${color_ojos_id}, ${color_piel_id}, ${contextura_id}, ${nivel_educacion_id}, ${estado_civil_id}, ${fumador_id}, ${bebedor_id}, $frecuencia_ejercicio_id, $salario_id, $altura, $peso, $num_hijos, '${quiere_hijos}')
+EOF
+done
+echo "SELECT 1 from DUAL;"
