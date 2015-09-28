3d14f54793f8417d40959c619299588f96c64ce8
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-09-27 21:50:14 -0600

    Relacion

---
+++ Scripts/DDL/3_Tablas/1_PersonaDisponible/9_Relacion.sql
@@ -0,0 +1,10 @@
+create table WINK
+(
+  persona1		VARCHAR2(10),
+  CONSTRAINT fk_wink_persona_emisor FOREIGN KEY (persona1) REFERENCES PERSONA(username),
+  persona2	 	VARCHAR2(10),
+  tipo_relacion_id	NUMBER,
+  CONSTRAINT fk_wink_persona_receptor FOREIGN KEY (tipo_relacion_id) REFERENCES TIPO_RELACION(tipo_relacion_id),
+  CONSTRAINT fk_wink_persona_receptor FOREIGN KEY (persona2) REFERENCES PERSONA(username),
+  CONSTRAINT pk_wink PRIMARY KEY (persona1, persona2)
+);
