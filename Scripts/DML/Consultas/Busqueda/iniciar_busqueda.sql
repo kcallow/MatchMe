44f0ee79d35687ba4df30e9e27cc4da6ba30e31c
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-09-27 20:20:37 -0600

    Inicio busqueda.

--- Scripts/DML/Consultas/Busqueda/iniciar_busqueda.sql
+++ Scripts/DML/Consultas/Busqueda/iniciar_busqueda.sql
@@ -1,33 +1,9 @@
-CREATE GLOBAL TEMPORARY TABLE TABLA_BUSQUEDA (
-  username		VARCHAR2(10),
-  es_admin		VARCHAR2(1),
-  nacimiento		DATE,
-  primer_apellido 	VARCHAR2(30),
-  segundo_apellido 	VARCHAR2(30),
-  nombre		VARCHAR2(40),
-  genero_id		NUMBER,
-  ciudad_id		NUMBER,
-  es_real		VARCHAR2(1),  
-  color_ojos_id 	NUMBER,      
-  color_piel_id 	NUMBER,      
-  contextura_id 	NUMBER,      
-  nivel_educacion_id 	NUMBER,      
-  estado_civil_id 	NUMBER,      
-  fumador_id 		NUMBER,      
-  bebedor_id 		NUMBER,      
-  frecuencia_ejercicio_id 	NUMBER,      
-  salario_id 		NUMBER,      
-  altura		NUMBER,
-  peso		NUMBER,
-  num_hijos	NUMBER,
-  quiere_hijos	VARCHAR2(1)
-)
 CREATE OR REPLACE PROCEDURE INICIAR_BUSQUEDA
 AS
 BEGIN
 insert into tabla_busqueda (username, es_admin, nacimiento, primer_apellido, segundo_apellido, nombre, genero_id, ciudad_id, slogan, es_real, color_ojos_id, color_piel_id, contextura_id, nivel_educacion_id, estado_civil_id, fumador_id, bebedor_id, frecuencia_ejercicio_id, salario_id, altura, peso, num_hijos, quiere_hijos)
-  SELECT (persona.username, es_admin, nacimiento, primer_apellido, segundo_apellido, nombre, genero_id, ciudad_id, slogan, es_real, color_ojos_id, color_piel_id, contextura_id, nivel_educacion_id, estado_civil_id, fumador_id, bebedor_id, frecuencia_ejercicio_id, salario_id, altura, peso, num_hijos, quiere_hijos)
-  FROM PERSONA INNER JOIN PERSONA_DISPONIBLE
+  SELECT persona.username, persona.es_admin, persona.nacimiento, persona.primer_apellido, persona.segundo_apellido, persona.nombre, persona.genero_id, persona.ciudad_id, persona.slogan, persona_disponible.es_real, persona_disponible.color_ojos_id, persona_disponible.color_piel_id, persona_disponible.contextura_id, persona_disponible.nivel_educacion_id, persona_disponible.estado_civil_id, persona_disponible.fumador_id, persona_disponible.bebedor_id, persona_disponible.frecuencia_ejercicio_id, persona_disponible.salario_id, persona_disponible.altura, persona_disponible.peso, persona_disponible.num_hijos, persona_disponible.quiere_hijos
+  FROM PERSONA, PERSONA_DISPONIBLE
   WHERE PERSONA.USERNAME = PERSONA_DISPONIBLE.USERNAME AND PERSONA_DISPONIBLE.ES_REAL = 1;
 END;
 /
---
+++ Scripts/DML/Consultas/Busqueda/tabla_busqueda.sql
@@ -0,0 +1,26 @@
+CREATE GLOBAL TEMPORARY TABLE TABLA_BUSQUEDA (
+  username		VARCHAR2(10),
+  es_admin		VARCHAR2(1),
+  nacimiento		DATE,
+  primer_apellido 	VARCHAR2(30),
+  segundo_apellido 	VARCHAR2(30),
+  nombre		VARCHAR2(40),
+  genero_id		NUMBER,
+  ciudad_id		NUMBER,
+  slogan		VARCHAR2(500),
+  es_real		VARCHAR2(1),  
+  color_ojos_id 	NUMBER,      
+  color_piel_id 	NUMBER,      
+  contextura_id 	NUMBER,      
+  nivel_educacion_id 	NUMBER,      
+  estado_civil_id 	NUMBER,      
+  fumador_id 		NUMBER,      
+  bebedor_id 		NUMBER,      
+  frecuencia_ejercicio_id 	NUMBER,      
+  salario_id 		NUMBER,      
+  altura		NUMBER,
+  peso		NUMBER,
+  num_hijos	NUMBER,
+  quiere_hijos	VARCHAR2(1)
+)
+/
